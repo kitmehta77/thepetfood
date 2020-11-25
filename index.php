1889296a3aa3d5c543103a2c28d1b06f364b7a69
kitmehta77 <kit.mehta@gmail.com>
2020-11-25 17:39:59 +1300

    css

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <div class = "header">
         <div class="mainimage"><img src = "images/mainimage.png" alt="Petfood Main Image"></div>
         <div class="logo"><img src="images/logo.png" alt="Petfood Logo"></div>
-        <div id="dogfp"><span style="alignment: center">The Pet Food Store</span></div>
+        <div id="dogfp"><span class="textmain">The Pet Food Store</span><span class="gradient"></span><span class="spotlight"></span> </div>
     </div>
 
 <div id = "nav">
